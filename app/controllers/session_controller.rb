440ac48b829ec3412408fb339fcc9cd7f36ce7e2
Gabor Ratky & Balint Erdi <dev+gr+be@secretsaucepartners.com>
2010-06-04 11:16:34 +0200

    [#3770072] Account names are listed on the dashboard
     * Improved PivotalTracker testability

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
   def create
     if warden.authenticate!
       store_user_handle(params[:username])
-      redirect_to root_path, :notice => "You are now logged in."
+      redirect_to dashboard_path, :notice => "You are now logged in."
     end
   end
 
--- lib/gitpit_pivotal_tracker.rb
+++ lib/gitpit_pivotal_tracker.rb
@@ -1,24 +1,51 @@
 # encoding: utf-8
 
 module GitPit
-  module PivotalTracker
-    def self.token=(value)
+  module RealPivotalTracker
+    def token=(value)
       ::PivotalTracker::Client.token = value
     end
     
-    def self.login(username, password)
+    def login(username, password)
       ::PivotalTracker::Client.token(username, password)
     end
     
-    def self.logout
+    def logout
       ::PivotalTracker::Client.token = nil
     end
     
-    def self.account_names
+    def account_names
       project_account_names = ::PivotalTracker::Project.all.map { |project| project.account }
       project_account_names.sort.uniq
-    end
-    
+    end    
   end
   
+  module FakePivotalTracker
+    
+    def token=(value)
+      @token = value
+    end
+    
+    def login(username, password)
+      @token = username
+    end
+    
+    def logout
+      @token = nil
+    end
+    
+    def account_names
+      []
+    end
+  end
+  
+  module PivotalTracker
+    extend RealPivotalTracker
+    
+    def self.mode=(mode)
+      extend FakePivotalTracker if mode == :test
+    end
+  end
+
 end
+

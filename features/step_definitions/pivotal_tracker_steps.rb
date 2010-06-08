ebadffcc650e967f4d3d30925c0d417d3cbe16b6
Gabor Ratky & Balint Erdi <dev+gr+be@secretsaucepartners.com>
2010-06-08 12:27:28 +0200

    Replaces Cucumber table hash keys with symbols for better readability

--- features/step_definitions/pivotal_tracker_steps.rb
+++ features/step_definitions/pivotal_tracker_steps.rb
@@ -9,16 +9,16 @@ end
 Given /^I have the following PT accounts:$/ do |table|
   accounts = {}
   table.hashes.each do |hash|
-    accounts[hash["name"]] ||= {}
-    if ( velocity = hash["overall_velocity"] )
-      accounts[hash["name"]]["overall_velocity"] = velocity
+    accounts[hash[:name]] ||= {}
+    if ( velocity = hash[:overall_velocity] )
+      accounts[hash[:name]][:overall_velocity] = velocity
     end
   end
   Gitpit::PivotalTracker.stub!(:account_names).and_return(accounts.keys)
   
   accounts.each do |account_name, account_data|
-    if account_data["overall_velocity"]
-      Gitpit::PivotalTracker.should_receive(:overall_velocity).with(account_name).and_return(account_data["overall_velocity"])
+    if account_data[:overall_velocity]
+      Gitpit::PivotalTracker.should_receive(:overall_velocity).with(account_name).and_return(account_data[:overall_velocity])
     end
   end
 end

bc05b10a1e23efc0aed231feab80de9b715e2687
Gabor Ratky & Balint Erdi <dev+gr+be@secretsaucepartners.com>
2010-06-08 14:36:32 +0200

    Simplifies stubbing of account names and account's overall velocity

--- features/step_definitions/pivotal_tracker_steps.rb
+++ features/step_definitions/pivotal_tracker_steps.rb
@@ -7,18 +7,12 @@ Given /^there is no PT profile with username: "([^\"]*)", password: "([^\"]*)"$/
 end
 
 Given /^I have the following PT accounts:$/ do |table|
-  accounts = {}
-  table.hashes.each do |hash|
-    accounts[hash[:name]] ||= {}
-    if ( velocity = hash[:overall_velocity] )
-      accounts[hash[:name]][:overall_velocity] = velocity
-    end
-  end
-  Gitpit::PivotalTracker.stub!(:account_names).and_return(accounts.keys)
-  
-  accounts.each do |account_name, account_data|
-    if account_data[:overall_velocity]
-      Gitpit::PivotalTracker.should_receive(:overall_velocity).with(account_name).and_return(account_data[:overall_velocity])
+  Gitpit::PivotalTracker.stub!(:account_names).and_return(table.hashes.map { |hash| hash[:name] })
+
+  if table.headers.include? "overall_velocity"
+    table.map_column!("overall_velocity") { |velocity| velocity.to_i }
+    table.hashes.each do |hash|
+      Gitpit::PivotalTracker.should_receive(:overall_velocity).with(hash[:name]).and_return(hash[:overall_velocity])
     end
   end
 end
